0d5dd7f73a2a5f705e4452c3fc97834047bd858f
ran-pe <itpe.youngran@gmail.com>
2022-06-06 01:28:34 +0900

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,46 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+			// https://prettier.io/docs/en/options.html
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto',
+			},
+		],
+		'vue/multi-word-component-names': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<p>App Header</p>
+		<demo></demo>
+	</div>
+</template>
+
+<script>
+import Demo from '@/demo/basic/Demo.vue';
+export default {
+	components: { Demo },
+};
+</script>
+
+<style></style>
