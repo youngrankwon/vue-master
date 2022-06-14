f57c6ce45a783abe547c32fa187f3a4946f330b1
youngran.kwon <youngran.kwon@gsitm.com>
2022-06-14 09:22:08 +0900

    7_til_list

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,42 @@
 	<div>
 		<div class="main list-container contents">
 			<h1 class="page-header">Today I Learned</h1>
+			<loading-spinner v-if="isLoading"></loading-spinner>
+			<ul v-else>
+				<post-list-item
+					v-for="postItem in postItems"
+					:key="postItem._id"
+					:postItem="postItem"
+				></post-list-item>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import PostListItem from '@/components/posts/PostListItem.vue';
+import { fetchPosts } from '@/api/index';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+export default {
+	components: { PostListItem, LoadingSpinner },
+	data() {
+		return {
+			postItem: [],
+			isLoading: false,
+		};
+	},
+	methods: {
+		async fetchData() {
+			this.isLoading = true;
+			const { data } = await fetchPosts();
+			this.isLoading = false;
+			this.postItems = data.posts;
+		},
+	},
+	created() {
+		this.fetchData();
+	},
+};
 </script>
 
 <style></style>
